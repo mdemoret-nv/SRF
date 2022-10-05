61bb634dc0840fb5de48d1bfa96d6b9550a81dc8
David Gardner <dagardner@nvidia.com>
2022-10-05 14:56:52 -0700

    Just use std thread

--- src/internal/runnable/rx_thread_factory.cpp
+++ src/internal/runnable/rx_thread_factory.cpp
@@ -1,15 +1,6 @@
 #include "srf/runnable/rx_thread_factory.hpp"
 
-#include "internal/system/resources.hpp"
-#include "internal/system/system.hpp"
-#include "internal/system/system_provider.hpp"
-#include "internal/system/thread.hpp"
-
-#include "srf/core/bitmap.hpp"
-#include "srf/options/options.hpp"
 #include "srf/runnable/thread_context.hpp"
-
-#include <boost/fiber/future/packaged_task.hpp>
 
 #include <functional>
 #include <memory>
@@ -37,23 +28,9 @@ class TaskWrapper
 
 std::thread srf_thread_factory(std::function<void()> task)
 {
-    auto options = std::make_unique<Options>();
-    options->engine_factories().set_default_engine_type(srf::runnable::EngineType::Thread);
-
-    auto srf_system           = srf::internal::system::make_system(std::move(options));
-    auto srf_system_provider  = srf::internal::system::SystemProvider(srf_system);
-    auto srf_system_resources = srf::internal::system::Resources::create(srf_system_provider);
-
     TaskWrapper tw(std::move(task));
     std::packaged_task<void()> pkg_task(std::move(tw));
-    std::future<void> future = pkg_task.get_future();
-
-    auto srf_thread = std::make_unique<srf::internal::system::Thread>(
-        srf_system_resources->make_thread("srf_thread_factory", srf_system->topology().cpu_set(), pkg_task));
-
-    std::thread thread = srf_thread->release();
-
-    return thread;
+    return std::thread(std::move(pkg_task));
 }
 
 }  // namespace srf::runnable
